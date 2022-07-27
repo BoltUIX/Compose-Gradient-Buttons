a10217d91c09c42b5e4a0a2faf9a33462434673a
BoltUIX <boltuix@gmail.com>
2022-07-27 11:23:03 +0530

    How to create Gradient Buttons | Shapes in Jetpack Compose

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Compose-Gradient-Buttons
-How to create Gradient Buttons in Jetpack Compose
+How to create Gradient Buttons | Shapes in Jetpack Compose
+
+* Gradient Button Style: top Start
+* Gradient Button Style: top End
+* Gradient Button Style: Bottom Start
+* Gradient Button Style: Bottom End
+* Gradient Button Style: Top Start & Bottom End
+* Gradient Button Style: Top End & Bottom Start
+* Gradient Button Style: All side
+* Gradient Button Disable
+* Gradient Button No Ripple
+
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjw3-0XeY2Mjrm2TP_q42e1tmnsx_gtyPfN1JNJcReHndopV5E__DxdNG1IWMTlRgfGiqol7YcOFfyHplxTHQ2RdMpkjoXffdqchLtD5uWktrIgbmMiewlaQpOuaB-6iAUNAc91P9kS880NvEI2-9whFzpAIGhNlCDYnxstZS7zbGV9ZKsiO4b5AQul/s16000/button.jpg" alt="Compose Gradient Button" width="100%">
+
+Our website:
+https://www.boltuix.com/2022/07/gradient-button-shapes-in-jetpack.html
---
+++ app/src/main/java/compose/material/theme/ui/theme/Color.kt
@@ -0,0 +1,76 @@
+package compose.material.theme.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/*
+https://www.boltuix.com/
+
+Build a custom color scheme to map dynamic color, use as fallback colors,
+or implement a branded theme. The color system automatically handles critical adjustments
+that provide accessible color contrast.
+
+https://material-foundation.github.io/material-theme-builder/#/custom
+*/
+
+val md_theme_light_primary = Color(0xFF9C404A)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFFFDADB)
+val md_theme_light_onPrimaryContainer = Color(0xFF40000D)
+val md_theme_light_secondary = Color(0xFF765658)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFFFDADB)
+val md_theme_light_onSecondaryContainer = Color(0xFF2C1517)
+val md_theme_light_tertiary = Color(0xFF76592F)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFDDB3)
+val md_theme_light_onTertiaryContainer = Color(0xFF291800)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF201A1A)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF201A1A)
+val md_theme_light_surfaceVariant = Color(0xFFF4DDDD)
+val md_theme_light_onSurfaceVariant = Color(0xFF524344)
+val md_theme_light_outline = Color(0xFF857373)
+val md_theme_light_inverseOnSurface = Color(0xFFFBEEED)
+val md_theme_light_inverseSurface = Color(0xFF362F2F)
+val md_theme_light_inversePrimary = Color(0xFFFFB2B7)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF9C404A)
+val md_theme_light_surfaceTintColor = Color(0xFF9C404A)
+
+val md_theme_dark_primary = Color(0xFF9C404A)
+val md_theme_dark_onPrimary = Color(0xFF5F121F)
+val md_theme_dark_primaryContainer = Color(0xFF7D2934)
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFDADB)
+val md_theme_dark_secondary = Color(0xFFE6BDBE)
+val md_theme_dark_onSecondary = Color(0xFF44292B)
+val md_theme_dark_secondaryContainer = Color(0xFF5C3F41)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFDADB)
+val md_theme_dark_tertiary = Color(0xFFE7C08E)
+val md_theme_dark_onTertiary = Color(0xFF432C06)
+val md_theme_dark_tertiaryContainer = Color(0xFF5C421A)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFDDB3)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF201A1A)
+val md_theme_dark_onBackground = Color(0xFFECE0DF)
+val md_theme_dark_surface = Color(0xFF201A1A)
+val md_theme_dark_onSurface = Color(0xFFECE0DF)
+val md_theme_dark_surfaceVariant = Color(0xFF524344)
+val md_theme_dark_onSurfaceVariant = Color(0xFFD7C1C2)
+val md_theme_dark_outline = Color(0xFF9F8C8D)
+val md_theme_dark_inverseOnSurface = Color(0xFF201A1A)
+val md_theme_dark_inverseSurface = Color(0xFFECE0DF)
+val md_theme_dark_inversePrimary = Color(0xFF9C404A)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFFFB2B7)
+val md_theme_dark_surfaceTintColor = Color(0xFFFFB2B7)
+
+
+val seed = Color(0xFF7A5557)
